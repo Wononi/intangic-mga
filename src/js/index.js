797879e5c7074e07fcba3b0cc07e5f0160bc4c36
Wononi <wononi02144@gmail.com>
2023-08-21 16:23:20 +0300

    Убирает лишнюю логику

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,4 @@ if (window.location.href === 'https://www.intangicmga.co/press' || window.locati
     location.replace("https://www.intangicmga.co/resources/press/031623");
 }
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            document.querySelector('.scroll__nav').classList.add('scroll__nav-show')
-        } else {
-            document.querySelector('.scroll__nav').classList.remove('scroll__nav-show')
-        }
-    });
-});
 
-observer.observe(document.querySelector('.navigation'))
